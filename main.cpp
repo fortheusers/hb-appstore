87b376bb10189f20bc957e3f01d1e6f97d154ec6
vgmoose <me@vgmoose.com>
2019-05-27 23:19:00 -0400

    switch+wiiu: use new repo urls

--- main.cpp
+++ main.cpp
@@ -17,9 +17,9 @@
 #endif
 
 #if defined(__WIIU__)
-#define DEFAULT_REPO "https://wiiubru.com/appstore"
+#define DEFAULT_REPO "https://wiiu.apps.fortheusers.org"
 #else
-#define DEFAULT_REPO "https://switchbru.com/appstore"
+#define DEFAULT_REPO "https://switch.apps.fortheusers.org"
 #endif
 
 int main(int argc, char* argv[])
