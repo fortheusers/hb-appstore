547c883562c74a68e908d4c3b67b42d7395b1601
GuestDreemurr <55953204+GuestDreemurr@users.noreply.github.com>
2022-11-03 20:39:24 +0000

    Update Sidebar.hpp

--- gui/Sidebar.hpp
+++ gui/Sidebar.hpp
@@ -33,11 +33,11 @@ public:
 	// list of human-readable category titles and short names from the json
 
 #if defined(__WIIU__)
-	const char* cat_names[TOTAL_CATS] = { "Search", "All Apps", "Aroma-Ready", "Games", "Emulators", "Tools", "Advanced", "Misc" };
-	const char* cat_value[TOTAL_CATS] = { "_search", "_all", "aroma", "game", "emu", "tool", "advanced", "_misc" };
+	const char* cat_names[TOTAL_CATS] = { "Search", "All Games", "Aroma-Ready Games", "Emulators", "Mods", "Games", "Misc" };
+	const char* cat_value[TOTAL_CATS] = { "_search", "_all", "aroma", "game", "emu", "mod", "_misc" };
 #else
-	const char* cat_names[TOTAL_CATS] = { "Search", "All Apps", "Games", "Emulators", "Tools", "Advanced", "Themes", "Misc" };
-	const char* cat_value[TOTAL_CATS] = { "_search", "_all", "game", "emu", "tool", "advanced", "theme", "_misc" };
+	const char* cat_names[TOTAL_CATS] = { "Search", "All Games", "Emulators", "Mods", "Games", "Misc" };
+	const char* cat_value[TOTAL_CATS] = { "_search", "_all", "game", "emu", "mod", "_misc" };
 #endif
 
 private:
